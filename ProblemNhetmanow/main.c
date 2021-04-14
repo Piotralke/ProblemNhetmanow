e230e7eee9e5999469b12ce0cd58fe8e8775cced
Piotralke <piotreq_46@wp.pl>
2021-04-14 17:39:19 +0200

    kod na 1 kamien

--- ProblemNhetmanow/main.c
+++ ProblemNhetmanow/main.c
@@ -39,27 +39,26 @@ void free_ptr(int** array, int size)
 	array = NULL;
 }
 
-/* A utility function to check if a queen can
-be placed on board[row][col]. Note that this
-function is called when "col" queens are
-already placed in columns from 0 to col -1.
-So we need to check only left side for
-attacking queens */
+/*
+Funkcja sprawdza czy hetman moze zostac postawiony na board[row][col].
+Funkcja jest przywolywana kiedy hetmany sa juz  w kolumnach od 0 do [col]-1.
+Wiec musimy sprawdzac tylko lewa strone dla tych hetmanow co atakuja.
+ */
 bool isSafe(int** board, int row, int col, int size)
 {
 	int i, j;
 
-	/* Check this row on left side */
+	/* Sprawdza rzad z lewej strony */
 	for (i = 0; i < col; i++)
 		if (board[row][i])
 			return false;
 
-	/* Check upper diagonal on left side */
+	/* Sprawdza gorna przekatna z lewej strony */
 	for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
 		if (board[i][j])
 			return false;
 
-	/* Check lower diagonal on left side */
+	/* Sprawdza dolna przekatna z lewej strony */
 	for (i = row, j = col; j >= 0 && i < size; i++, j--)
 		if (board[i][j])
 			return false;
@@ -67,58 +66,43 @@ bool isSafe(int** board, int row, int col, int size)
 	return true;
 }
 
-/* A recursive utility function to solve N
-Queen problem */
-bool solveNQUtil(int** board, int col, int size)
+/* Funkcja rekurencyjna rozwiazujaca problem N hetmanow */
+bool solveNhetman(int** board, int col, int size)
 {
-	/* base case: If all queens are placed
-	then return true */
+	/* glowny warunek: jezeli wszystkie chetmany sa ustawione zwroc true */
 	if (col >= size)
 		return true;
 
-	/* Consider this column and try placing
-	this queen in all rows one by one */
+	/* Sprawdza kolumne i sprawdza czy moze postawic hetmana we wszystkich wierszach po kolei */
 	for (int i = 0; i < size; i++) {
-		/* Check if the queen can be placed on
-		board[i][col] */
+		/* Sprawdza czy hetman moze zostac postawiony w board[i][col]] */
 		if (isSafe(board, i, col, size)) {
-			/* Place this queen in board[i][col] */
+			/* Stawia hetmana w board[i][col] */
 			board[i][col] = 1;
-			//print_array(board, size);
-			//puts("");
-			/* recur to place rest of the queens */
-			
-			if (solveNQUtil(board, col + 1, size))
+
+			/* stawia pozostale hetmany */
+
+			if (solveNhetman(board, col + 1, size))
 			{
 				counter++;
 				printf("Rozwiazanie %d:\n", counter);
 				print_array(board, size);
 				puts("");
 			}
-			
 
-			/* If placing queen in board[i][col]
-			doesn't lead to a solution, then
-			remove queen from board[i][col] */
-			board[i][col] = 0; // BACKTRACK
+
+			/* Jezeli stawianie hetmanow na planszy nie prowadzi do rozwiazania, usuwa hetmany z planszy  */
+			board[i][col] = 0;
 
 		}
-		
+
 	}
 
-	/* If the queen cannot be placed in any row in
-		this colum col then return false */
+	/* Jezeli hetman nie moze zostac postawiony w zadnym wierszu w tej kolumnie zwraca false */
 	return false;
 }
 
-/* This function solves the N Queen problem using
-Backtracking. It mainly uses solveNQUtil() to
-solve the problem. It returns false if queens
-cannot be placed, otherwise, return true and
-prints placement of queens in the form of 1s.
-Please note that there may be more than one
-solutions, this function prints one of the
-feasible solutions.*/
+
 
 
 
@@ -138,8 +122,8 @@ int main()
 	else
 	{
 		fill_array(array, size);
-		if (solveNQUtil(array, 0, size) == false) {
-			printf("Ilosc rozwiazan dla rozmiaru %d: %d",size, counter);
+		if (solveNhetman(array, 0, size) == false) {
+			printf("Ilosc rozwiazan dla rozmiaru %d: %d", size, counter);
 			free_ptr(array, size);
 			return false;
 		}
